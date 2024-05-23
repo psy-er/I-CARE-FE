795cdcd7a85692eafc973e2d3fd4299a9777e6c7
lsa0828 <lsa0828@duksung.ac.kr>
2024-05-23 13:58:26 +0900

    Navbar, App 수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import DiaryDetail from "./feat_diary/DiaryDetail";
+import DiaryCalendar from "./feat_diary/DiaryCalendar";
+import AddDiary from "./feat_diary/AddDiary";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Routes>
+      <Route path="/diary" element={<DiaryCalendar />} />
+      <Route path="/diary/detail" element={<DiaryDetail />} />
+      <Route path="/diary/add" element={<AddDiary />} />
+    </Routes>
   );
 }
 
