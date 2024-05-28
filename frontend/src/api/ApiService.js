62df3ac5cb5e26ec6a58c219debdfe1242f03ef3
psy-er <89902211+psy-er@users.noreply.github.com>
2024-05-28 22:23:23 +0900

    feat : 부모 로그인 구현 완료

--- frontend/src/api/ApiService.js
+++ frontend/src/api/ApiService.js
@@ -4,12 +4,11 @@ export function call(api, method, request) {
   let headers = new Headers({
     "Content-Type": "application/json",
   });
-  /*
+  
   const accessToken = localStorage.getItem("ACCESS_TOKEN");
-  if(accessToken && accessToken !== null) {
+  if(accessToken) {
     headers.append("Authorization", "Bearer " + accessToken);
-  }*/
-  headers.append("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJsc2FAbmF2ZXIiLCJpc3MiOiJzb2NpYWwgTG9naW4iLCJpYXQiOjE3MTY4NzgyMjgsImV4cCI6MTcxNjk2NDYyOCwiaWQiOiIyYzk0OTNmNzhmYmRlYTg1MDE4ZmJkZWFhYTI4MDAwMCJ9.nQbXvc99BfbLiiHVC5obNCsXIKY36alkHTvQX1tGA7wkqZcca6yM_TAstWVPpQLxE1JL7fxjuMOY9j78c7i4Dw");
+  }
 
   let options = {
     headers: headers,
@@ -34,14 +33,15 @@ export function call(api, method, request) {
       } else {
         return response.json();
       }
-    } /*else if(response.status === 403) {
+    } else if(response.status === 403) {
       window.location.href = "/login"; // redirect
     } else {
       Promise.reject(response);
       throw Error(response);
-    }*/
+    }
   }).catch((error) => {
     console.log("http error");
     console.log(error);
   });
+
 }
