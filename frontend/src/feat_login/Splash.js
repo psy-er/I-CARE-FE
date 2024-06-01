64c293eadbcd190f3353a79c447601ae6e68122c
psy-er <89902211+psy-er@users.noreply.github.com>
2024-06-02 01:51:09 +0900

    feat : splash 화면 배치를 위한 preview 컴포넌트 생성

--- frontend/src/feat_login/Splash.js
+++ frontend/src/feat_login/Splash.js
@@ -3,27 +3,27 @@ import { useNavigate } from 'react-router-dom';
 import './css/Splash.css'; // CSS 파일을 추가합니다.
 
 const Splash = () => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      navigate('/login'); // 3초 후에 로그인 페이지로 이동
-    }, 3000);
-
-    return () => {
-      clearTimeout(timer);
-    };
-  }, [navigate]);
-
-  return (
-    <div className="splash-screen">
-      <img
-        src={`${process.env.PUBLIC_URL}/splash.png`}
-        alt="Splash"
-        className="splash-image" // 추가: 이미지에 클래스 적용
-      />
-    </div>
-  );
-};
-
-export default Splash;
+    const navigate = useNavigate();
+  
+    useEffect(() => {
+      const timer = setTimeout(() => {
+        navigate('/login'); // 3초 후에 로그인 페이지로 이동
+      }, 3000);
+  
+      return () => {
+        clearTimeout(timer);
+      };
+    }, [navigate]);
+  
+    return (
+      <div className="splash-screen">
+        <img
+          src={`${process.env.PUBLIC_URL}/splash.png`}
+          alt="Splash"
+          className="splash-image" // 추가: 이미지에 클래스 적용
+        />
+      </div>
+    );
+  };
+  
+  export default Splash;
