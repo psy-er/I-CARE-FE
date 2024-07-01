5eb8132cb6a1a82221d98a5664fe07cef0099811
lsa0828 <lsa0828@duksung.ac.kr>
2024-07-01 21:09:13 +0900

    profile state 추가, 프론트 개선

--- frontend/src/PageFirst.js
+++ frontend/src/PageFirst.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Navbar from "./Navbar";
 import { useNavigate } from "react-router-dom";
